1097a553b062631075c2c6f860e727e43b5e0649
Nikhil Panwar <nikhilpanwar4455@gmail.com>
2024-01-05 23:20:04 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,12 @@
+// const reverse = str => str.split('').reverse().join('');
+
+// console.log(reverse("Hello \nWorld"));
+
+let dailyActivities = ['eat','sleep'];
+
+dailyActivities[2] = 'exercise' ;
+
+dailyActivities.pop();
+
+console.log(dailyActivities.indexOf('sleep'));
+
