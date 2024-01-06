71d21b3c1b08eea13783031e378b9f3527f734c6
Nikhil Panwar <nikhilpanwar4455@gmail.com>
2024-01-06 23:40:15 +0530

    fourth commit

--- new.js
+++ new.js
@@ -7,3 +7,10 @@ let dailyActivities = ['eat','sleep'];
 dailyActivities[2] = 'exercise' ;
 
 console.log(dailyActivities);
+
+let comma = 3 
+let minus = 2
+
+let both = comma - minus;
+
+console.log(both);
