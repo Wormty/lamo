35f79e616de364e06fdedda5da929b472eb33cbf
Nikhil <nikhilpanwar4455@gmail.com>
2024-01-11 23:47:45 +0530

    9th commit

--- new.js
+++ new.js
@@ -15,3 +15,11 @@ let both = comma + minus;
 
 console.log(both);
 
+
+const myArray = ['h', 'e', 'l', 'l', 'o'];
+
+// first element
+console.log(myArray[0]);  // "h"
+
+// second element
+console.log(myArray[1]); // "e"
